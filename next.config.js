44d4294759565632ecb9b775d4dc8f3faf6074f6
well <the.w.3107@gmail.com>
2025-03-23 02:37:56 -0300

    fix: python config

--- next.config.js
+++ next.config.js
@@ -7,14 +7,6 @@ const nextConfig = {
   env: {
     base_api_url: base_api_host + "/api",
   },
-  rewrites: async () => {
-    return [
-      {
-        source: "/api/:path*",
-        destination: base_api_host + "/api/" + api_mapper,
-      },
-    ];
-  },
   webpack(config) {
     // Grab the existing rule that handles SVG imports
     const fileLoaderRule = config.module.rules.find((rule) =>

392a7eb19ea9eefb44f48df57e831ff740a079cd
well <the.w.3107@gmail.com>
2025-03-23 02:06:21 -0300

    feat: hide users endpoints

--- api/routes/users.py
+++ api/routes/users.py
@@ -8,32 +8,32 @@ from api.lib.auth import private_route
 NAMESPACE = "Usuarios"
 api = create_api_blueprint(NAMESPACE)
 
-@api.route("/me")
-class UserController(MethodView):
-    @api.doc(operationId="getCurrentUserData")
-    @private_route()
-    def get(self):
-        print('\n\n\n\n\n callback', g.current_user)
-        return jsonify({})
+# @api.route("/me")
+# class UserController(MethodView):
+#     @api.doc(operationId="getCurrentUserData")
+#     @private_route()
+#     def get(self):
+#         print('\n\n\n\n\n callback', g.current_user)
+#         return jsonify({})
 
 
-@api.route("/sign-up")
-class SignUpController(MethodView):
-    @api.arguments(LoginRequestSchema)
-    @api.doc(operationId="createAccount")
-    def post(self, body):
-        response = supabase_client.auth.sign_up(body)
-        return {}
+# @api.route("/sign-up")
+# class SignUpController(MethodView):
+#     @api.arguments(LoginRequestSchema)
+#     @api.doc(operationId="createAccount")
+#     def post(self, body):
+#         response = supabase_client.auth.sign_up(body)
+#         return {}
 
 
-@api.route("/login")
-class LoginController(MethodView):
-    @api.arguments(LoginRequestSchema)
-    @api.doc(operationId="login")
-    def post(self, body):
-        response = supabase_client.auth.sign_in_with_password(body)
+# @api.route("/login")
+# class LoginController(MethodView):
+#     @api.arguments(LoginRequestSchema)
+#     @api.doc(operationId="login")
+#     def post(self, body):
+#         response = supabase_client.auth.sign_in_with_password(body)
 
-        if not response.session:
-            return {}
+#         if not response.session:
+#             return {}
 
-        return { 'access_token': response.session.access_token, 'refresh_token': response.session.refresh_token }
+#         return { 'access_token': response.session.access_token, 'refresh_token': response.session.refresh_token }
